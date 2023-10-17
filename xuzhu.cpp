5e76bd457121be24239fd18b732a55d98e2bf18c
tangxitao <2682585843@qq.com>
2023-10-17 10:03:24 +0800

    22

---
+++ xuzhu.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello Tangxitao!\n");
+    printf("萧远山222\n");
+    return 0;
+}
+
+
---
+++ yangxiao.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello Tangxitao!\n");
+    printf("陈家洛\n");
+    return 0;
+}
+
+
